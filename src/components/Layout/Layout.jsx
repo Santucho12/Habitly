1f88eec67725643e32cf4af768debdcdeeb12931
Santucho12 <santysegal@gmail.com>
2026-01-22 04:59:42 -0300

    terminando detalles

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -23,11 +23,13 @@ export default function Layout({ children }) {
       <div className="flex-1 flex flex-col">
         <Topbar />
         <main className="flex-1 p-4 overflow-y-auto bg-gray-900">
-          {activityName && (
-            <h1 className="text-2xl font-bold mb-6 text-white">{activityName}</h1>
-          )}
-          <div key={location.pathname} className="transition-all duration-500 ease-in-out animate-fadein">
-            {children}
+          <div className="max-w-md mx-auto">
+            {activityName && (
+              <h1 className="text-2xl font-bold mb-6 text-white">{activityName}</h1>
+            )}
+            <div key={location.pathname} className="transition-all duration-500 ease-in-out animate-fadein">
+              {children}
+            </div>
           </div>
         </main>
       </div>
